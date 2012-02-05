a885b4b0dee6d90ea22d1a1a1bdcbedd1a1aa9ed
femto <femtowin@gmail.com>
2012-02-05 13:45:54 +0800

    inital commit

---
+++ lib/heroku_development_email_enabling.rb
@@ -0,0 +1,5 @@
+require "heroku_development_email_enabling/version"
+
+module HerokuDevelopmentEmailEnabling
+  # Your code goes here...
+end
---
+++ lib/heroku_development_email_enabling/version.rb
@@ -0,0 +1,3 @@
+module HerokuDevelopmentEmailEnabling
+  VERSION = "0.0.1"
+end
